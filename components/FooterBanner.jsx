2d74cc69aa757cb069d0c541ccccc66700db8467
jaafar2000 <jaafar200021@gmail.com>
2022-11-21 18:31:55 +0530

    first commit

---
+++ components/FooterBanner.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Link from "next/link";
+import { urlFor } from "../lib/client";
+
+const FooterBanner = ({
+  footerBanner: {
+    discount,
+    largeText1,
+    largeText2,
+    midText,
+    smallText,
+    product,
+    buttonText,
+    image,
+    saleTime,
+    desc,
+  },
+}) => {
+  return (
+    <div className="footer-banner-container">
+      <div className="banner-desc">
+        <div className="left">
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+        <div className="right">
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
+        </div>
+
+        <img 
+        src={urlFor(image)} 
+        alt=""
+        className="footer-banner-image"
+        />
+        
+      </div>
+    </div>
+  );
+};
+
+export default FooterBanner;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import React from "react";
+import { Toaster } from "react-hot-toast"
+import "../styles/globals.css";
+import { Layout } from "../components";
+import { StateContext } from "../context/StateContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
